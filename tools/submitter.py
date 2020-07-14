b3768cee57349a6280eafc951afe7df855aa2010
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2020-07-15 03:18:19 +0530

    A quick and hacky Flag Submit Script

---
+++ tools/submitter.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import ctfd
+from creds import *
+import os
+
+ctf = ctfd.CTFd(URL, {"pass": password, "user": username})
+ctf.auth()
+
+challs = ctf.challenges()
+pwd = os.getcwd()
+with open(pwd + "/.challenge_name") as f:
+    identifier = f.read()
+with open(pwd + "/flag_txt") as f:
+    flag = f.read().strip("\n").strip(" ")
+print("[*] " + identifier + ": " + flag + " Submitting Flag", end="")
+res = challs[identifier].submit(flag)
+if res:
+    print("\r[ + ] " + identifier + ": " + flag + " Accepted        ", end="")
+else:
+    print("\r[ - ] " + identifier + ": " + flag + " Rejected       ", end="")
+
+
